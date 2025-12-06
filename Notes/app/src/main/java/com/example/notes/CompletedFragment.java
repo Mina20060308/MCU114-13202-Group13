70c5837b90f1f7cd777685124ac1738702516fda
張卉妤 <13130941@me.mcu.edu.tw>
2025-12-06 15:09:30 +0800

    登入介面

--- Notes/app/src/main/java/com/example/notes/CompletedFragment.java
+++ Notes/app/src/main/java/com/example/notes/CompletedFragment.java
@@ -19,13 +19,13 @@ public class CompletedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        // 連結 XML layout
+
         View view = inflater.inflate(R.layout.fragment_completed, container, false);
 
-        // 找到 ListView
+
         ListView listView = view.findViewById(R.id.listCompleted);
 
-        // 模擬完成項目（之後可以換成資料庫）
+
         String[] completedItems = {"買早餐", "寫功課", "練習 Fragment"};
 
         ArrayAdapter<String> adapter =
