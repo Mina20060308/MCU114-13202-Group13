1f5c96ee0ffad6b42c4aee922c2be10003219024
yunzhen94 <zyun67601@gmail.com>
2025-12-10 01:11:53 +0800

    新增事項

--- Notes/app/src/main/java/com/example/notes/AddTaskFragment.java
+++ Notes/app/src/main/java/com/example/notes/AddTaskFragment.java
@@ -1,64 +1,63 @@
 package com.example.notes;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddTaskFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.Calendar;
+
 public class AddTaskFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private EditText etTime1, etTime2, etTime3;
 
     public AddTaskFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddTaskFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddTaskFragment newInstance(String param1, String param2) {
-        AddTaskFragment fragment = new AddTaskFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_add_task, container, false);
+
+        etTime1 = view.findViewById(R.id.etTime1);
+        etTime2 = view.findViewById(R.id.etTime2);
+        etTime3 = view.findViewById(R.id.etTime3);
+
+        View.OnClickListener listener = v -> showTimePicker((EditText) v);
+
+        etTime1.setOnClickListener(listener);
+        etTime2.setOnClickListener(listener);
+        etTime3.setOnClickListener(listener);
+
+        return view;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private void showTimePicker(EditText targetEditText) {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_task, container, false);
+        TimePickerDialog dialog = new TimePickerDialog(
+                getContext(),
+                (picker, hourOfDay, minuteSelected) -> {
+                    String selectedTime = String.format("%02d:%02d", hourOfDay, minuteSelected);
+                    targetEditText.setText(selectedTime);
+                },
+                hour,
+                minute,
+                true
+        );
+
+        dialog.show();
     }
 }
